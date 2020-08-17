08ddb2de04c1277db3867cd61922dad904501dd5
dsantos22 <dsantos@ibscards.com.au>
2020-08-17 16:20:35 +0800

    Updates

---
+++ precache-manifest.e2df5b11971d2e98be90085b7c0bf3a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f249d8286258874dc2f5464a0692482",
+    "url": "/wisdompetmed-dada/index.html"
+  },
+  {
+    "revision": "45dae5ad8a0ccaa47277",
+    "url": "/wisdompetmed-dada/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "b236bf3a4d0215bc267a",
+    "url": "/wisdompetmed-dada/static/css/main.963435ea.chunk.css"
+  },
+  {
+    "revision": "45dae5ad8a0ccaa47277",
+    "url": "/wisdompetmed-dada/static/js/2.0e674e0a.chunk.js"
+  },
+  {
+    "revision": "12f5fece24482f5e949c183d0aad3b8d",
+    "url": "/wisdompetmed-dada/static/js/2.0e674e0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b236bf3a4d0215bc267a",
+    "url": "/wisdompetmed-dada/static/js/main.8ad28013.chunk.js"
+  },
+  {
+    "revision": "ba0328721c00e2beea30",
+    "url": "/wisdompetmed-dada/static/js/runtime-main.db631ce3.js"
+  }
+]);
